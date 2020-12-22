d802341509459c7d59e59c313354a26b5698ff89
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-22 22:28:56 +0300

    9

--- 9/main.cpp
+++ 9/main.cpp
@@ -1,14 +1,57 @@
-//
-//  main.cpp
-//  9
-//
-//  Created by Никита Кузнецов on 22.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+1 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int time;
+    cin >> time;
+    cout << time % 60;
+}
+2 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int k;
+    cin >> k;
+    cout << k % 7;
+}
+3 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int k, n;
+    cin >> k;
+    cin >> n;
+    int x = (k + n) % 7;
+    if (x - 1 == 0) cout << 7;
+    else
+        cout << x-1;
+}
+4 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int A = a / c;
+    int B = b / c;
+    int S = A * B;
+    int s = S / c * c;
+    cout << s;
+    int ost = a * b - (A*c*B*c);
+    cout << "\n" << ost;
+}
+5 задание:
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int year;
+    cin >> year;
+    int ans = year / 100;
+    if (year % 100) ans++;
+    cout << ans;
 }
